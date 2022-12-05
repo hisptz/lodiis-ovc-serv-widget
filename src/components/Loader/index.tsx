5146b46dcc04e23c7f2f03a23b1e8f575f4245b9
gnnko <nnkogift@hotmail.com>
2022-12-05 16:49:34 +0300

    fix(Loader): Improvements
    - Centered loading indicator and text

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -1,15 +1,13 @@
-import {CenteredContent, CircularLoader, LinearLoader} from "@dhis2/ui";
+import {CircularLoader, LinearLoader} from "@dhis2/ui";
 
 
 export default function Loader({progress, text}: { progress?: number, text?: string }) {
 
 
     return (
-        <div style={{minHeight: 300}}>
-            <CenteredContent>
-                {progress ? <LinearLoader amount={progress}/> : <CircularLoader small/>}
-                {text && <h3>{text}</h3>}
-            </CenteredContent>
+        <div className="column center align-items-center" style={{minHeight: 600}}>
+            {progress ? <LinearLoader amount={progress}/> : <CircularLoader small/>}
+            {text && <h3>{text}</h3>}
         </div>
     )
 }
