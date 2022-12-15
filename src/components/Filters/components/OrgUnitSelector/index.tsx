1c11c5f9ff7e0896174a3f93b53e686165ac2ade
gnnko <nnkogift@hotmail.com>
2022-12-15 12:37:35 +0300

    chore(*): Format & cleanup
    - Cleaned up uneeded code

--- src/components/Filters/components/OrgUnitSelector/index.tsx
+++ src/components/Filters/components/OrgUnitSelector/index.tsx
@@ -1,33 +1,26 @@
 import {SingleSelectField, SingleSelectOption} from "@dhis2/ui";
 import {useRecoilState, useRecoilValue} from "recoil";
 import {AccessibleOrgUnits, OrgUnitFilterState} from "../../state";
-import {head} from "lodash";
-import {LOWEST_LEVEL} from "../../../../constants";
 import {OrganisationUnit} from "@hisptz/dhis2-utils";
 
 
 export default function OrgUnitSelector() {
     const [selectedOrgUnit, setSelectedOrgUnit] = useRecoilState(OrgUnitFilterState);
     const orgUnits = useRecoilValue(AccessibleOrgUnits);
-    const ouLevel = head(orgUnits)?.level ?? 0;
 
     return (
         <>
-            {
-                ouLevel < LOWEST_LEVEL && (
-                    <SingleSelectField
-                        label="Organisation unit"
-                        selected={selectedOrgUnit?.id}
-                        onChange={({selected}) => setSelectedOrgUnit(orgUnits?.find(({id}) => id === selected) as OrganisationUnit)}
-                        filterable>
-                        {
-                            orgUnits?.map(({name, id}) => (
-                                <SingleSelectOption key={`${id}-option`} label={name} value={id}/>
-                            ))
-                        }
-                    </SingleSelectField>
-                )
-            }
+            <SingleSelectField
+                label="Organisation unit"
+                selected={selectedOrgUnit?.id}
+                onChange={({selected}) => setSelectedOrgUnit(orgUnits?.find(({id}) => id === selected) as OrganisationUnit)}
+                filterable>
+                {
+                    orgUnits?.map(({name, id}) => (
+                        <SingleSelectOption key={`${id}-option`} label={name} value={id}/>
+                    ))
+                }
+            </SingleSelectField>
         </>
     )
 }
