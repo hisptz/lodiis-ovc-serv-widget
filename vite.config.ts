23ea27c1738bf27075910120a89f47382482cf05
Davis799 <davistibz@gmail.com>
2023-08-30 15:45:59 +0300

    chore(package.json): updated app packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
-import {defineConfig} from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "./",
-    plugins: [react()],
-    build: {
-        outDir: "build",
-        copyPublicDir: true,
-        target: "modules"
-    }
+  base: "./",
+  plugins: [react()],
+  build: {
+    outDir: "build",
+    copyPublicDir: true,
+    target: "modules",
+  },
 })
